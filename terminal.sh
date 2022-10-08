4b00ba928e086912216c726d4026cf866ca8af0c
Daniel Garcia <112222815+DanielGarcia654@users.noreply.github.com>
2022-10-08 01:44:17 -0500

    [AGREGANDO][terminal.sh] Subiendo terminal
    Subo una "plantilla" de la terminal, falta agregar los comandos requeridos del proyecto

---
+++ terminal.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./login2.sh
+clear
+inicio=1;
+quiensoy=$(whoami)
+dir=$(pwd)
+
+#./login2.sh Para llamar otros scripts
+
+carpeta=${dir##*/} #Para imprimir unicamente la carpeta (##*/ para limitar todo lo que esta antes de la barra -/-, como si se imprimiera unicamente el nombre de un archivo)
+texto="$quiensoy - $carpeta "
+while [ $inicio -eq 1 ]
+do
+	echo -n "$texto"; read comando #leemos la cadena de texto que ingresara el usuario para interpretarla en la terminal
+	if [[ "$comando" == "salir" ]]
+	then
+		inicio=0; clear
+	elif [[ "$comando" == "reproductor" ]]
+	then
+		echo "prueba ian"
+	else
+		$comando #Para usar los comandos predeterminados de Linux
+	fi
+done
