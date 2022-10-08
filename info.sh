935949944d12661230960d7ac25cdd2834325791
Ian LG <112226789+ianlg99@users.noreply.github.com>
2022-10-07 23:20:41 -0500

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-echo "Memoria RAM: "
-cat /proc/meminfo
 echo
-echo "Version del sistema operativo: " 
+printf "> RAM Total:                        "
+cat /proc/meminfo | grep "MemTotal" | cut --complement -c -17
+echo
+
+printf "> RAM Usada:                        "
+cat /proc/meminfo | grep "MemAvailable" | cut --complement -c -17
+echo
+
+printf "> ID Fabricante:                    "
+cat /proc/cpuinfo | grep "vendor" | cut --complement -c -12
+echo
+
+printf "> Procesador:                      "
+cat /proc/cpuinfo | grep "name" | cut --complement -c -12
+echo
+
+printf "> Arquitectura del procesador:      " 
+cat /etc/apt/sources.list | grep "Linux" | cut -d ' ' -f8
+echo
+
+printf "> Version del sistema operativo:    " 
 cat /etc/issue.net
+echo
