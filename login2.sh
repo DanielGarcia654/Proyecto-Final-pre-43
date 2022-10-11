5c3fd1207a4a50ab5add9dd8553b3811a36fbd2e
Daniel Garcia <112222815+DanielGarcia654@users.noreply.github.com>
2022-10-10 22:13:34 -0500

    [TERMINADO] Agregando la version final del login
    Subiendo la version final del login, cambie de posicion el inicio de la terminal y viceversa (antes iniciabamos la terminal primero y luego el login)

--- login2.sh
+++ login2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 #Con getent, encontramos si existe un usuario con las credenciales ingresadas y comparamos dependiendo del resultado
-
+clear
 echo "Ingresa tu usuario: "
 read usuario
-if [ $(getent passwd $usuario | wc -l) -eq 1 ] #wc cuenta cuantos archivos hay de estos
-then 
+if [ $(getent passwd $usuario | wc -l) -eq 1 ] #wc cuenta cuantos archivos hay de estos, lo podemos usar para comparar la cantidad
+then
 	echo "Ingresa tu contrasena: "
-	read -s contrasena #Comparamos la contrasena ingresada con un logging que realizamos para ver si es exitoso el login
-	if [ $(echo "$contrasena" | su -l "$usuario" | wc -l) -eq 0 ]
+	contrasena=$(su -c echo "$usuario")
+	#usamos un pequeno login y dependiendo del estado(0-exitoso, 1-error) comparamos para continuar el programa, la -c es para que se ejecute un comando (en este caso un echo vacio) junto al usuario
+	if [ $? -eq 0 ] #$?: es el estado del comando ejecutado actual (nuestro comando es el su 
 	then
 		clear
 		echo "Adelante padre"
+		chmod +x ./terminal.sh
+		./terminal.sh #llamamos el script de la terminal si el loging es exitoso
 	else
 		echo "Contrasena incorrecta"
 		exit
