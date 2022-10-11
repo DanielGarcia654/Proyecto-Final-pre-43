a24d4cc9b4ecbf941f6dc57e8e15398d305f1d4c
Daniel Garcia <112222815+DanielGarcia654@users.noreply.github.com>
2022-10-10 22:14:19 -0500

    [MODIFICANDO] Agregando una version de la terminal
    Agregando una version de la terminal (aun no se termina)

--- terminal.sh
+++ terminal.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-chmod +x ./login2.sh
-#./login2.sh
+trap ' ' 2 15 20 #Para evitar que el usuario salga de programa con CTRL+Z o CTRL+C
 clear
-inicio=1;
+inicio=1
 quiensoy=$(whoami)
 dir=$(pwd)
 
 #./login2.sh Para llamar otros scripts
 
 carpeta=${dir##*/} #Para imprimir unicamente la carpeta (##*/ para limitar todo lo que esta antes de la barra -/-, como si se imprimiera unicamente el nombre de un archivo)
-texto="$quiensoy - $carpeta "
+
 while [ $inicio -eq 1 ]
 do
-	echo -n "$texto"; read comando #leemos la cadena de texto que ingresara el usuario para interpretarla en la terminal
+	dir=$(pwd) #Llamamos estas dos variables de nuevo para actualizar dinamicamente la carpeta
+	carpeta=${dir##*/} #Actualiza la carpeta actual
+	echo -n "$(tput bold setaf 5)$quiensoy - $(tput bold setaf 6)$carpeta$(tput sgr0): "; read comando #leemos la cadena de texto que ingresara el usuario para interpretarla en la terminal
+
 	if [[ "$comando" == "salir" ]]
 	then
 		inicio=0; clear
@@ -21,6 +23,16 @@ do
 	then
 		chmod +x ./reproductor.sh
 		./reproductor.sh
+	elif [[ "$comando" == "ayuda" ]]
+	then
+		echo "Estos son los comandos disponibles: "
+		echo "    infosis - despliega la informacion del sistema"
+		echo "    fecha - despliega la fecha y hora actual"
+		echo "    buscar - busca un archivo en un directorio especifico (parametros: carpeta y archivo a buscar"
+		echo "    juego - despliega un juego (ahorcado)"
+		echo "    reproductor - abre el reproducto de musica"
+		echo "    creditos - creditos de los creadores"
+		echo "    salir - para salir de la terminal"
 	else
 		$comando #Para usar los comandos predeterminados de Linux
 	fi
